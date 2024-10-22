e14068aa41a206a4e6aab69812bd630e643f3f8d
ameysunu <ameysunu00719@gmail.com>
2024-10-22 21:33:03 +0100

    ui changes

--- PhishTank/Views/HomeView.swift
+++ PhishTank/Views/HomeView.swift
@@ -12,11 +12,50 @@ struct HomeView: View {
     
     var user: GIDGoogleUser?
     
+    let items: [(String, String)] = [("Phishing", "Check for phishing texts or emails"), ("Breaches", "Check if your email has been in a potential breach"), ("Recents", "Your recent activities")]
+    let columns = [
+           GridItem(.flexible()),
+           GridItem(.flexible()),
+           GridItem(.flexible())
+       ]
+    
     var body: some View {
         VStack(alignment: .leading){
             Text("Hello, \(user?.profile?.name ?? "User")")
                 .font(.title)
+            ScrollView{
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(items, id: \.0){ item in
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color.gray.opacity(0.25))
+                            VStack (alignment: .leading, spacing: 5){
+                                Text(item.0)
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                Text(item.1)
+                                    .font(.caption)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                            .aspectRatio(1, contentMode: .fit)
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+            }
+            .padding()
             Spacer()
+            
+            HStack{
+                    Image("Gemini")
+                        .resizable()
+                        .frame(width: 75, height: 46.375)
+                
+                Text("Results generated with Google Gemini")
+                    .font(.system(size: 12))
+                    .foregroundColor(.gray)
+                    .padding(.top, 10)
+            }
         }
         .padding()
     }
