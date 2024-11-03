3d489ed18513caeda15cbb06fd109a4ca079107d
ameysunu <ameysunu00719@gmail.com>
2024-11-03 18:57:39 +0000

    breach factor from gemini added

--- PhishTank/Views/Widgets/Breach.swift
+++ PhishTank/Views/Widgets/Breach.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct Breach: View {
     var dismiss: () -> Void
     var _widController = WidgetController()
+    var _geminiController = GeminiController()
     @State var email: String = ""
     @State var isLoading: Bool = false
     @State var breachData: [BreachAnalyticsResponse.ExposedBreaches.BreachDetail]? = []
     @State var isResponse: Bool = false
+    @State var breachMeasures: String = ""
+    @State var isBreachMeasureClicked: Bool = false
     
     let columns = [
         GridItem(.flexible()),
@@ -63,69 +66,91 @@ struct Breach: View {
                         .font(.title2)
                         .foregroundStyle(.green)
                 } else {
-                    Text("Ohh no! Breaches have been detected on your email address")
-                        .font(.title2)
-                        .foregroundStyle(.red)
                     
-                    ScrollView{
-                        ForEach(breachData!, id: \.breach){ item in
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 15)
-                                    .fill(Color.gray.opacity(0.25))
-                                HStack{
-                                    AsyncImage(url: URL(string: item.logo ?? "https://upload.wikimedia.org/wikipedia/commons/5/53/Sheba1.JPG")) { image in
-                                        image
-                                            .resizable()
-                                            .clipShape(Circle())
-                                            .overlay {
-                                                Circle().stroke(Color.white, lineWidth: 4)
-                                            }
-                                            .shadow(radius: 7)
-                                    } placeholder: {
-                                        ProgressView()
-                                    }
-                                    .frame(width: 100, height: 100)
-                                    .padding()
-                                    
-                                    VStack (alignment: .leading, spacing: 5){
-                                        Text(item.breach!)
-                                            .font(.title)
-                                            .foregroundColor(.white)
-                                        Text(item.details!)
-                                            .font(.caption)
-                                            .foregroundColor(.secondary)
-                                        
-                                        HStack{
-                                            Text(item.domain!)
-                                                .font(.caption)
-                                                .foregroundColor(.secondary)
-                                            Spacer()
-                                            Text("Exposed Date: \(item.xposed_date!)")
-                                                .font(.caption)
-                                                .foregroundColor(.secondary)
-                                        }
-                                    }
-                                    .padding()
-                                }
-                            }
-                            .frame(maxWidth: .infinity)
-                            .fixedSize(horizontal: false, vertical: true)
-                        }
-                    }
-                    
-                    Button(action: {
+                    if(!breachMeasures.isEmpty){
                         
-                    }){
-                        Text("Learn how to make your account more secure")
+                        ScrollView{
+                            
+                            Text(breachMeasures)
+                                .font(.caption)
+                                .foregroundColor(.secondary)
+                        }
+                        
+                    } else {
+                        
+                        Text("Ohh no! Breaches have been detected on your email address")
+                            .font(.title2)
+                            .foregroundStyle(.red)
+                        
+                        ScrollView{
+                            ForEach(breachData!, id: \.breach){ item in
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 15)
+                                        .fill(Color.gray.opacity(0.25))
+                                    HStack{
+                                        AsyncImage(url: URL(string: item.logo ?? "https://upload.wikimedia.org/wikipedia/commons/5/53/Sheba1.JPG")) { image in
+                                            image
+                                                .resizable()
+                                                .clipShape(Circle())
+                                                .overlay {
+                                                    Circle().stroke(Color.white, lineWidth: 4)
+                                                }
+                                                .shadow(radius: 7)
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                                        .frame(width: 100, height: 100)
+                                        .padding()
+                                        
+                                        VStack (alignment: .leading, spacing: 5){
+                                            Text(item.breach!)
+                                                .font(.title)
+                                                .foregroundColor(.white)
+                                            Text(item.details!)
+                                                .font(.caption)
+                                                .foregroundColor(.secondary)
+                                            
+                                            HStack{
+                                                Text(item.domain!)
+                                                    .font(.caption)
+                                                    .foregroundColor(.secondary)
+                                                Spacer()
+                                                Text("Exposed Date: \(item.xposed_date!)")
+                                                    .font(.caption)
+                                                    .foregroundColor(.secondary)
+                                            }
+                                        }
+                                        .padding()
+                                    }
+                                }
+                                .frame(maxWidth: .infinity)
+                                .fixedSize(horizontal: false, vertical: true)
+                            }
+                        }
+                        
+                        Button(action: {
+                            Task {
+                                var breachDetails = ""
+                                
+                                for data in breachData! {
+                                    breachDetails.append(data.details!)
+                                }
+                                
+                                breachMeasures = await _geminiController.getBreachMeasures(prompt: breachDetails)
+                                print(breachMeasures)
+                            }
+                        }){
+                            Text("Learn how to make your account more secure")
+                        }
+                        
                     }
                     
-                }
-                
-                Button(action:{
-                    isResponse = false
-                    isLoading = false
-                }){
-                    Text("Check another email")
+                    Button(action:{
+                        isResponse = false
+                        isLoading = false
+                    }){
+                        Text("Check another email")
+                    }
                 }
             }
             
