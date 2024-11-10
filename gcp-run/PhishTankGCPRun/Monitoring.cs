4b412b104bc39619019965f803481104b4dc3f9f
ameysunu <ameysunu00719@gmail.com>
2024-11-10 13:05:43 +0000

    gemini sdk added

--- gcp-run/PhishTankGCPRun/Monitoring.cs
+++ gcp-run/PhishTankGCPRun/Monitoring.cs
@@ -1,14 +1,19 @@
-﻿using Google.Cloud.Firestore;
+﻿using DotnetGeminiSDK.Client;
+using DotnetGeminiSDK.Client.Interfaces;
+using DotnetGeminiSDK.Config;
+using Google.Cloud.Firestore;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using System.Text;
 
 namespace PhishTankGCPRun
 {
     public class Monitoring
     {
-        public async Task StartMonitoringEngine()
+        public async Task<string> StartMonitoringEngine()
         {
-            var fireStoreKeyBase64 = "ewogICJ0eXBlIjogInNlcnZpY2VfYWNjb3VudCIsCiAgInByb2plY3RfaWQiOiAiaW9zLXByb2plY3QtMTFjMzQiLAogICJwcml2YXRlX2tleV9pZCI6ICJkMzcxMWFhNjJjMGE4MzQyNmYwZWYwZWEzMzJhNWZjZmVkZTY3MGI5IiwKICAicHJpdmF0ZV9rZXkiOiAiLS0tLS1CRUdJTiBQUklWQVRFIEtFWS0tLS0tXG5NSUlFdlFJQkFEQU5CZ2txaGtpRzl3MEJBUUVGQUFTQ0JLY3dnZ1NqQWdFQUFvSUJBUUMzSlhnR0xEaDQweDFhXG5JWVlpWDVZNnNGRTIwYmFxSFFCME1XaTZmcm9mUldweVZFU1RYS3ZSLzRKRy9RQjVDcjZHWUtuMzRlZVFveUJiXG51MHl2eVdoT2Z5cGlxSFNwaElycHF1NlRRcVhnOHRIRUV5VXhPWkx0WkJZZkFnREh3Lzlib1NZSDh0dmlEVG1OXG5DdUhqN2gxRGY3eFBaMU9yazExSzBMQlJCSUMzQ010ZXVjdVkwSENJcUppVU85MjBxaVk4TTJGTUltT3dUeUU0XG51Tm00by9VUithR3hLSVdxSElhcHFVUHNJcGRCMk5pejBmYTd1Q1ZNbVU2aWg1ZHdCL1hROWZGY3c5b1RSaGZRXG4wL1Jxc0NaYXFlbzIzMXoyWlN3djFXSkVCdjNjdmtvUTVXMGw3WFEzV0srK2c3YkpuRlE1OXNRRzhUeWZMY3dPXG5ITE1DRVJKcEFnTUJBQUVDZ2dFQUY5SGV1aEYyOHMxUHR2dVVqTlNaSHdtZTRSNEdGT1ZGWnRkUG1LOHlHUzV0XG5Ec1pEbzZyYzMxTjNwOXRGL0NMWDdzcnBzRWRkclE1dnFmUHRTeEQxNlAwRVpUcDY3eS9zT3BjMFdrdHVkRU1BXG5qSnVLTGN4MWZlYytNcUE1dThHVHdtRERmU3Nxbi9ibEI2RmxDY0VHdTFNRXRJc1ZjeGp0SnZXSGM3SVRPanJKXG5RZG1JZ3dKQkxmZjU1bWx5TWtxSnB0eUJSVE4xUlpsMnpqeWd2SElRTnJ2NHFZMVhCeHFqKzI3allHMXBhMyt5XG51TUs1UiswOUhkdVZwaHF5eWVxWjJtZm1Gc0tnUks5Yk53emxBcmtxWTYvMXUrT1VDcGE2Y0ZVSy9GSC9GU1lSXG5ESzBIWjRCYVRmKzB2WWJNbXlTaTY2WkNBMENtM2pqU3RZV2NLc1ArMVFLQmdRRGJHWDcvVkZ4VXRxbS9RNnZpXG5TT2JjRzFqNjhVeklmU0p5Y29oZU1vTjZQbEc5aXdUSTlocG45YytLOFlWanVDMFJWSUwwWVh4WWJCanAzNW0yXG5UM05oajZ3R3RzaEpCY0RBcWVjNjZrMmpQbVlPT2lnMDdWUUlOZHJ1UVNpZnlzamtibTBCWUpUZTVnRFRaR1FiXG4rTjVlWENWWEdvU3d5WDFZMXNqaVNkRHNSd0tCZ1FEVi9kaFlkYjhUOUdaWm5rOWtCRzhMdkZXbFByRFBkUlpPXG5Jamg3ZmQrZ1pUbWsrTUlVUDc4Y2h5STYvSnpZY29CUUhRcEtRN0ZGcHAvNzg3bGM5WWVwVVpXcTVYa2d4Q3RGXG5ldFlqSDJQQUN6bXMxcHBZaVF5QkkrUXdPOGljV0tvSkl6bFJMTUt0NVNGYldWT01zMzNiRzRycWJpNWFKdmg1XG5SY3RSN01GVHp3S0JnUURJNS8wekpta3Y2UHVJZmdZY0RsdGJFOXlvNXlGUWFxWnVxYVZ3TXdPcTBZQmt2UmhiXG5Lb2lXTWFucEw0VHdKczkwcDdrWlhGY0lRYjhxbmJXMm5lNGdWUDBhZnZyek1zcElJTVArbXlFc0FxalVmUEduXG5ic1J5YnVmZE4zU0g2Z3dDbzF0ak91dm41S0ozTlFRelBpYXlBZTdmaTFxSFBZdXFMZkd3eHAxRU93S0JnQ05OXG5xYStPWFdPeFltRk9tTkpyV05hYXo1WmZiVlFNZ1EySzM3NmYyWkRnb2szeExET2pBS2g0TXRHQWMyL1NGQkVqXG5lVnNmSjlQUjFYci9qb2tqaUtvS1o2SzJFUEd5NjJDZkQwemtGTlRPQlVuYWJjUkZkK2FtUVRNajZjakJaK3Z4XG4vN2JBcDYyQXE2d2laSUpGNm5HSGJiZVhUZFpacjR4eVUwVEV4bkJKQW9HQUNHdHFpcnFPQkt6dy9SN0pVL1JzXG5makovMnNoRnF3M2JMdFNjS3RLNDd1c2FFcUxtMmZ1WUZtT1FacFA0Qk1iNmUvdTNsUlVTc09zZ3RqQXNpT0Q1XG5CM3hQcGZSQ3VielRQL2N1aDBxT2E2bDQyNW5XNkcwMkVqUXNReFRSY25mQjg2YkpUaXRHN1JhVzMrQ25Bd1o4XG5nelhqbFZ0ZjlOMXA2OEZSTVVuWXMzYz1cbi0tLS0tRU5EIFBSSVZBVEUgS0VZLS0tLS1cbiIsCiAgImNsaWVudF9lbWFpbCI6ICJmaXJlYmFzZS1hZG1pbnNkay01dGd2Y0Bpb3MtcHJvamVjdC0xMWMzNC5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsCiAgImNsaWVudF9pZCI6ICIxMDE0MzYyMDMwOTAxODM1MTE4OTMiLAogICJhdXRoX3VyaSI6ICJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20vby9vYXV0aDIvYXV0aCIsCiAgInRva2VuX3VyaSI6ICJodHRwczovL29hdXRoMi5nb29nbGVhcGlzLmNvbS90b2tlbiIsCiAgImF1dGhfcHJvdmlkZXJfeDUwOV9jZXJ0X3VybCI6ICJodHRwczovL3d3dy5nb29nbGVhcGlzLmNvbS9vYXV0aDIvdjEvY2VydHMiLAogICJjbGllbnRfeDUwOV9jZXJ0X3VybCI6ICJodHRwczovL3d3dy5nb29nbGVhcGlzLmNvbS9yb2JvdC92MS9tZXRhZGF0YS94NTA5L2ZpcmViYXNlLWFkbWluc2RrLTV0Z3ZjJTQwaW9zLXByb2plY3QtMTFjMzQuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLAogICJ1bml2ZXJzZV9kb21haW4iOiAiZ29vZ2xlYXBpcy5jb20iCn0K";
+            var fireStoreKeyBase64 = Environment.GetEnvironmentVariable("FIREBASE_SECRET_KEY");
             var serviceAccountKey = Encoding.UTF8.GetString(Convert.FromBase64String(fireStoreKeyBase64));
 
             Firebase firebaseCtrl = new Firebase();
@@ -43,6 +48,17 @@ namespace PhishTankGCPRun
                     }
                 }
             }
+
+            var geminiConfig = new GoogleGeminiConfig
+            {
+                ApiKey = Environment.GetEnvironmentVariable("FIREBASE_SECRET_KEY")
+            };
+
+            var geminiClient = new GeminiClient(geminiConfig);
+            GeminiProcessor _gemProc = new GeminiProcessor(geminiClient);
+            var prompt = "Helllooo";
+            return await _gemProc.GeminiRun(prompt);
+
 
         }
 
@@ -100,9 +116,8 @@ namespace PhishTankGCPRun
             try
             {
                 Monitoring mtr = new Monitoring();
-                await mtr.StartMonitoringEngine();
+                return await mtr.StartMonitoringEngine();
 
-                return "Success";
             } catch (Exception ex)
             {
                 return BadRequest(ex.Message);
